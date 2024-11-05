acb63743ad67feb4b2bd09f709a0b9b05af5d357
Alexandr Zhelanov <AlexandrZhelanov@gmail.com>
2024-11-06 02:26:51 +0300

    Testing image sending

--- tServer.js
+++ tServer.js
@@ -1,12 +1,18 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
 import 'dotenv/config'
-import express from 'express'
 import mysql from 'mysql2'
-import bodyParser from 'body-parser'
+import schedule from 'node-schedule'
+import { parse } from 'path'
+import express from 'express'
 
 // Setup Connections
 const bot = new Telegraf(String(process.env.BOT_TOKEN))
+const app = express();
+app.use(express.static('public'))
+app.listen(3000, function () {
+  console.log('Listening on http://localhost:3000/');
+});
 const mysqlConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -32,17 +38,23 @@ mysqlConnection.connect((err) => {
   }
 })
 
+// Listeners
 bot.on(message('text'), async (ctx) => {
-  getMessage(ctx)
+  console.log(`Chat id is: ${ctx.message.chat.id}`)
+  getMessage()
 })
 
-function getMessage(ctx) {
+const job = schedule.scheduleJob('* * * * *', function() {
+  getMessage()
+})
+
+// Internal Functions
+function getMessage() {
   mysqlConnection.query(
     "SELECT * FROM content",
     (err, results, fields) => {
       if (!err) {
-        console.log(results[0].Message)
-        sendToBot(ctx, results[0].Message)
+        sendToBot(process.env.CHAT_ID, results[0].Message)
       } else {
         console.log(err)
       }
@@ -50,6 +62,18 @@ function getMessage(ctx) {
   )
 }
 
-async function sendToBot(ctx, message) {
-  await ctx.sendMessage(`Message is: ${message}`)
+function sendToBot(chatId, message) {
+  bot.telegram.sendMessage(chatId, `Message from DB is: ${message}`)
+  .then(() => {
+    bot.telegram.sendMessage(process.env.CHAT_ID, "Message and etc...[\u200B](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8lF2jbNFBy7X4D6F43tRiCxG2oRWLP9v8LQ&s)", {
+      //bot.telegram.sendMessage(process.env.CHAT_ID, "Message and etc...[\u200B](https://i.ibb.co/nBPqbmD/1.png)", {
+      parse_mode: "markdown"
+    })
+    .then(() => {
+      bot.telegram.sendQuiz(process.env.CHAT_ID, "Hello?", ["1", "2"], {
+        correct_option_id: 0,
+        explanation: "Правильное слово - воть"
+      })
+    })
+  })
 }
