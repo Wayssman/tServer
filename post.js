28e034ced24c663a1e8459f70bcd383bf02c0fbf
Alexandr Zhelanov <AlexandrZhelanov@gmail.com>
2024-11-21 22:41:39 +0300

    Add link button to post Added header to post Added emojis to post title Added uppercase for post title

--- post.js
+++ post.js
@@ -3,6 +3,11 @@ import * as dbFunctions from './Database/databaseRequests.js'
 import * as coreErrors from './Utilities/coreErrors.js'
 import { getWordArgument } from './Utilities/coreUtilities.js'
 import { makeSafe } from './Utilities/telegramUtilities.js'
+import { Markup } from 'telegraf'
+
+const postFooterButtons = Markup.inlineKeyboard([
+    [Markup.button.url('Повторяй слова в боте!', 'https://t.me/QuizMasterskaya_bot')]
+])
 
 export async function handleAdminMessage(chatId, text) {
     try {
@@ -43,7 +48,8 @@ async function assemblePost(chatId, word) {
 
         // Отсылаем пост в бот
         await bot.telegram.sendMessage(chatId, postMessage, {
-            parse_mode: "MarkdownV2"
+            parse_mode: "MarkdownV2",
+            reply_markup: postFooterButtons.reply_markup
         })
     } catch (error) {
         await bot.telegram.sendMessage(chatId, coreErrors.getErrorDescription(error))
@@ -52,11 +58,12 @@ async function assemblePost(chatId, word) {
 }
 
 function getPostMessage(word) {
-    const titleMessage = `*${makeSafe(`${word.title}`)}* \n\n`
+    const headerMessage = "📭 *Новое слово на сегодня:* \n\n"
+    const titleMessage = `*${makeSafe(`📚 ${word.title.toUpperCase()} 📚`)}* \n\n`
     const postMessage = makeSafe(word.message)
     const imageMessage = `[\u200B](${word.image})`
 
-    const fullMessage = titleMessage + postMessage + imageMessage
+    const fullMessage = headerMessage + titleMessage + postMessage + imageMessage
     return fullMessage
 }
 
@@ -84,7 +91,8 @@ export async function assembleScheduledPost(chatId) {
   
       // Отсылаем пост в бот
       await bot.telegram.sendMessage(chatId, postMessage, {
-        parse_mode: "MarkdownV2"
+        parse_mode: "MarkdownV2",
+        reply_markup: postFooterButtons.reply_markup
     })
   
       dbFunctions.setChannelCounter(chatId, wordId + 1)
